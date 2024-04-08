718f4639cbdbb91bd6f74f090a308a61f553f8be
Rafael Ventura <raf4ael@gmail.com>
2024-04-08 01:02:08 -0300

    REFORMATACAO DO ARQUIVO DE formatacao_dados.py E JUNCAO DE RESPONSABILIDADES COM classificacao.py

--- src/scripts/formatacao_dados.py
+++ src/scripts/formatacao_dados.py
@@ -1,46 +1,55 @@
 import numpy as np
 import pandas as pd
 import unidecode
-from .classificacao import classificar_forma_ingresso, classificar_forma_evasao, classificar_idade, arredondar_cra
+
+from src.scripts.distancia import calcular_distancia_ate_urca, adicionar_distancia_ate_urca
 
 
+# Função para ler os dados brutos da planilha
 def ler_dados():
     caminho_arquivo = r'C:\Dev\dashboard-bsi\dados\bruto\planilhaJoinCriptografada.xlsx'
-    dataframe = pd.read_excel(caminho_arquivo)
-
-    # Converter colunas de data para o tipo datetime logo após a leitura
-    dataframe['DT_NASCIMENTO'] = pd.to_datetime(dataframe['DT_NASCIMENTO'], errors='coerce')
-    dataframe['DT_EVASAO'] = pd.to_datetime(dataframe['DT_EVASAO'], errors='coerce')
-    return dataframe
+    df = pd.read_excel(caminho_arquivo)
+    df['DT_NASCIMENTO'] = pd.to_datetime(df['DT_NASCIMENTO'], errors='coerce')
+    df['DT_EVASAO'] = pd.to_datetime(df['DT_EVASAO'], errors='coerce')
+    return df
 
 
-def remover_colunas(dataframe):
+def remover_colunas(df):
+    """
+    # Função para remover colunas desnecessárias do DataFrame.
+    """
     colunas_desnecessarias = ['Unnamed: 0', 'ID_PESSOA', 'NOME_PESSOA', 'MATR_ALUNO', 'CPF_MASCARA']
-    return dataframe.drop(colunas_desnecessarias, axis=1)
+    return df.drop(colunas_desnecessarias, axis=1)
 
 
-def preencher_nulos(dataframe):
-    substituicoes = {
-        'BAIRRO': 'Desconhecido',
-        'CIDADE': 'Desconhecido',
-        'ESTADO': 'Desconhecido'
-    }
-    return dataframe.fillna(substituicoes)
+def preencher_nulos(df):
+    """
+    Função para preencher valores nulos nas colunas de bairro, cidade e estado.
+    """
+    substituicoes = {'BAIRRO': 'Desconhecido',
+                     'CIDADE': 'Desconhecido',
+                     'ESTADO': 'Desconhecido'}
+    return df.fillna(substituicoes)
 
 
-def formatar_periodos(dataframe):
+def formatar_periodos(df):
+    """
+    Função para formatar os períodos de ingresso e evasão, separando o ano e o semestre.
+    """
     for periodo in ['PERIODO_EVASAO', 'PERIODO_INGRESSO']:
-        dataframe_temp = pd.DataFrame(dataframe[periodo].str.split('/', expand=True))
-        dataframe_temp.columns = ['ANO', periodo]
-        dataframe_temp[periodo] = dataframe_temp[periodo].str.extract('(\d+)', expand=False)
-        dataframe['PER_' + periodo + '_FORMAT'] = dataframe_temp['ANO'] + '.' + dataframe_temp[periodo]
-        dataframe[periodo] = dataframe_temp[periodo]
-        dataframe['ANO_' + periodo] = dataframe_temp['ANO']
-    return dataframe
+        df_temp = pd.DataFrame(df[periodo].str.split('/', expand=True))
+        df_temp.columns = ['ANO', periodo]
+        df_temp[periodo] = df_temp[periodo].str.extract('(\d+)', expand=False)
+        df[f'{periodo}_FORMATADO'] = df_temp['ANO'] + '.' + df_temp[periodo]
+        df[periodo] = df_temp[periodo]
+        df[f'ANO_{periodo}'] = df_temp['ANO']
+    return df
 
 
-# TODO: implementar no arquivo formatacao_dados uma conversão do campo ANO_PERIODO_INGRESSO de float para date.year
-def converter_tipos(dataframe):
+def converter_tipos(df):
+    """
+    Função para converter os tipos das colunas do DataFrame.
+    """
     tipo_campos = {
         'SEXO': str,
         'DT_NASCIMENTO': 'datetime64[ns]',
@@ -50,57 +59,53 @@ def converter_tipos(dataframe):
         'DT_EVASAO': 'datetime64[ns]',
         'PERIODO_EVASAO': np.float64,
         'ANO_PERIODO_EVASAO': np.float64,
-        'PER_PERIODO_EVASAO_FORMAT': str,
+        'PERIODO_INGRESSO_FORMATADO': str,
         'ANO_PERIODO_INGRESSO': np.float64
     }
-    dataframe['CRA'] = dataframe['CRA'].str.replace(',', '.').astype(float)
-    return dataframe.astype(tipo_campos)
+
+    df['CRA'] = df['CRA'].str.replace(',', '.').astype(float)
+    return df.astype(tipo_campos)
 
 
-def salvar_dados(dataframe):
-    dataframe.to_csv(r'C:\Dev\dashboard-bsi\dados\processado\dfPrincipal.csv', index=False)
-
-
-def limpar_bairros(dataframe):
-    dataframe['BAIRRO'] = dataframe['BAIRRO'].apply(lambda bairro: unidecode.unidecode(bairro).lower().strip())
-
-    correcoes_bairros = {
-        'freguesia.*': 'freguesia (jacarepagua)',
-
-    }
+def limpar_bairros(df):
+    """
+    Função para limpar e normalizar os nomes dos bairros.
+    """
+    df['BAIRRO'] = df['BAIRRO'].apply(lambda bairro: unidecode.unidecode(bairro).lower().strip())
+    correcoes_bairros = {'freguesia.*': 'freguesia (jacarepagua)'}
     for padrao, correcao in correcoes_bairros.items():
-        dataframe['BAIRRO'] = dataframe['BAIRRO'].str.replace(padrao, correcao, regex=True, case=False)
-    return dataframe
+        df['BAIRRO'] = df['BAIRRO'].str.replace(padrao, correcao, regex=True)
+    return df
 
 
-def adicionar_cidade_estado(dataframe):
+def adicionar_cidade_estado(df):
+    """
+    Função para adicionar informações de cidade e estado com base nos bairros do Rio de Janeiro.
+    """
     bairros_rj = [
         'tijuca', 'jardim botanico', 'santa teresa', 'leme', 'copacabana',
         'meier', 'vila da penha', 'botafogo', 'icarai', 'laranjeiras',
         'catete', 'rocinha', 'olaria', 'flamengo', 'madureira', 'urca', 'rocha'
     ]
 
-    # Atualizar cidade e estado para registros com bairros específicos e cidade/estado desconhecidos
     for bairro in bairros_rj:
-        dataframe.loc[(dataframe['BAIRRO'] == bairro) & (
-                (dataframe['CIDADE'] == 'Desconhecido') | (dataframe['ESTADO'] == 'Desconhecido')), ['CIDADE',
-                                                                                                     'ESTADO']] = [
+        df.loc[(df['BAIRRO'] == bairro) & (
+                (df['CIDADE'] == 'Desconhecido') | (df['ESTADO'] == 'Desconhecido')), ['CIDADE', 'ESTADO']] = [
             'Rio de Janeiro', 'Rio de Janeiro']
 
-    # Tratamento especial para 'Jardim Olavo Bilac' e 'Gramacho', que estão em Duque de Caxias
     bairros_duque_caxias = ['jardim olavo bilac', 'gramacho']
     for bairro in bairros_duque_caxias:
-        dataframe.loc[(dataframe['BAIRRO'] == bairro) & (
-                (dataframe['CIDADE'] == 'Desconhecido') | (dataframe['ESTADO'] == 'Desconhecido')), ['CIDADE',
-                                                                                                     'ESTADO']] = [
+        df.loc[(df['BAIRRO'] == bairro) & (
+                (df['CIDADE'] == 'Desconhecido') | (df['ESTADO'] == 'Desconhecido')), ['CIDADE', 'ESTADO']] = [
             'Duque de Caxias', 'Rio de Janeiro']
 
-    dataframe.loc[dataframe['BAIRRO'] == 'centro/nova iguacu', 'CIDADE'] = 'Nova Iguaçu'
-
-    return dataframe
+    return df
 
 
-def normalizar_bairros(dataframe):
+def normalizar_bairros(df):
+    """
+    Função para normalizar os nomes dos bairros.
+    """
     correcoes = {
         'cosme velh': 'cosme velho',
         'iraja!': 'iraja',
@@ -131,33 +136,100 @@ def normalizar_bairros(dataframe):
         'quintino': 'quintino bocaiuva',
         'laranjeira': 'laranjeiras',
         'bonsuceso': 'bonsucesso',
-        'braz de pina': 'bras de pina',
-
+        'braz de pina': 'bras de pina'
     }
-
-    dataframe['BAIRRO'] = dataframe['BAIRRO'].apply(lambda b: correcoes.get(b, b))
-    return dataframe
-
-
-def classificar(dataframe):
-    print("Iniciando classificação dos dados...")
-    # Classifica a forma de ingresso
-    dataframe = classificar_forma_ingresso(dataframe)
-
-    # Classifica a forma de evasão e faz ajustes adicionais, incluindo ANO_EVASAO
-    dataframe = classificar_forma_evasao(dataframe)
-    # Classifica a idade
-    dataframe = classificar_idade(dataframe)
-
-    # Arrendonda o CRA
-    dataframe = arredondar_cra(dataframe)
-
-    return dataframe
+    df['BAIRRO'] = df['BAIRRO'].apply(lambda b: correcoes.get(b, b))
+    return df
 
 
-# Adicione as funções que você já definiu aqui
+def arredondar_cra(df):
+    """
+    Função para arredondar o CRA dos alunos para a escala de 0.5.
+    """
+    df['CRA_arredondado'] = (df['CRA'] * 2).round() / 2
+    return df
+
+
+def classificar_idade(df):
+    """
+    Função para classificar a idade dos alunos no momento do ingresso.
+    """
+    df['IDADE'] = df.apply(lambda row: calcular_idade_ingresso(row), axis=1)
+    return df
+
+
+def calcular_idade_ingresso(row):
+    """
+    Função para calcular a idade do aluno no momento do ingresso.
+    :param row:
+    :return:
+    """
+    ano_ingresso, semestre_ingresso = map(int, row['PERIODO_INGRESSO_FORMATADO'].split('.'))
+    mes_ingresso = 1 if semestre_ingresso == 1 else 7
+    data_ingresso = pd.Timestamp(year=ano_ingresso, month=mes_ingresso, day=1)
+    idade_ingresso = data_ingresso.year - row['DT_NASCIMENTO'].year - (
+            (data_ingresso.month, data_ingresso.day) < (row['DT_NASCIMENTO'].month, row['DT_NASCIMENTO'].day))
+    return idade_ingresso
+
+
+def classificar_forma_ingresso(df):
+    """
+    Função para classificar os alunos em cotistas, ampla concorrência e outros.
+    """
+    cotas = ['SISU Escola Publica - Indep. de Renda', 'SISU Escola Publica até 1,5 S.M.']
+    ampla_concorrencia = ['VE - Vestibular', 'EN - ENEM', 'SISU Ampla Concorrencia']
+    df['FORMA_INGRESSO_SIMPLES'] = df['FORMA_INGRESSO'].apply(
+        lambda x: 'Cotas' if x in cotas else 'Ampla Concorrência' if x in ampla_concorrencia else 'Outros')
+    return df
+
+
+def classificar_forma_evasao(df):
+    """
+    Função para classificar os alunos em Evasão, Concluído ou Cursando.
+    """
+    condicoes = [
+        df['FORMA_EVASAO'].str.contains('ABA|APO|Desistência SiSU|JUB|CAN', na=False),
+        df['FORMA_EVASAO'].str.contains('CON', na=False),
+        df['FORMA_EVASAO'].str.contains('Sem evasão', na=False)
+    ]
+    categorias = ['Evasão', 'Concluído', 'Cursando']
+    df['STATUS_EVASAO'] = np.select(condicoes, categorias, default='Evasão')
+    return df
+
+
+def extrair_data_ingresso(row):
+    """
+    Função para extrair a data de ingresso do aluno.
+    """
+    ano_ingresso, semestre_ingresso = map(int, row['PERIODO_INGRESSO_FORMATADO'].split('.'))
+    mes_ingresso = 1 if semestre_ingresso == 1 else 7
+    return pd.Timestamp(year=ano_ingresso, month=mes_ingresso, day=1)
+
+
+def calcular_tempo_curso(df):
+    """
+    Função para calcular o tempo de curso dos alunos concluídos.
+    """
+    df_concluidos = df[df['STATUS_EVASAO'] == 'Concluído'].copy()
+    df_concluidos['TEMPO_CURSO'] = df_concluidos.apply(lambda row: ((extrair_data_ingresso(row).year - int(
+        row['ANO_PERIODO_INGRESSO'])) * 12 + extrair_data_ingresso(row).month - (
+                                                                        1 if row['PERIODO_INGRESSO_FORMATADO'].endswith(
+                                                                            '.1') else 7)) // 6, axis=1)
+    df['TEMPO_CURSO'] = df_concluidos['TEMPO_CURSO']
+    return df
+
+
+def salvar_dados(df):
+    """
+    Função para salvar o DataFrame formatado em um arquivo CSV.
+    """
+    df.to_csv(r'C:\Dev\dashboard-bsi\dados\processado\dfPrincipal.csv', index=False)
+
 
 def formatar():
+    """
+    Função principal para formatar e classificar os dados brutos.
+    """
     df = ler_dados()
     df = remover_colunas(df)
     df = preencher_nulos(df)
@@ -166,7 +238,12 @@ def formatar():
     df = limpar_bairros(df)
     df = adicionar_cidade_estado(df)
     df = normalizar_bairros(df)
-    df = classificar(df)
+    df = classificar_idade(df)
+    df = classificar_forma_ingresso(df)
+    df = classificar_forma_evasao(df)
+    df = arredondar_cra(df)
+    df = calcular_tempo_curso(df)
+    df = adicionar_distancia_ate_urca(df)
     salvar_dados(df)
     print('DataFrame formatado, classificado e salvo com sucesso!')
     return df
